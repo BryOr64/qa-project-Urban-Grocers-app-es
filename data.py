5ab83e54d80701e85c7d82d669e082f5ee14d462
Bryan Orosco <bryor64@gmail.com>
2024-05-29 00:49:31 -0400

    pruebas positivas con exito

---
+++ data.py
@@ -0,0 +1,41 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Max",
+    "phone": "+12345678912",
+    "address": "8042 Lancaster Ave.Hamburg, NY"
+}
+
+product_ids = {
+    "ids": [1, 2, 3]
+}
+
+kit_body = {
+    "name": "asdf"
+}
+
+kit_body_class = {
+    "kit1": {"name": "a"},
+    "kit2": {"name": "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"
+                     "cdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"
+                     "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"
+                     "cdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"
+                     "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"
+                     "cdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcda"
+                     "bcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"},
+    "kit3": {"name": ""},
+    "kit4": {"name": "Abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc"
+                     "dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"
+                     "cdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcda"
+                     "bcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd"
+                     "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabc"
+                     "dabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdab"
+                     "cdabcdabcdabcdabcdabcdabcdabcdabcdabcD"},
+    "kit5": {"name": "№#"},
+    "kit6": {"name": "A Aaa"},
+    "kit7": {"name": "123"},
+    "kit8": {"name": ""},
+    "kit9": {"name": 123}
+}
